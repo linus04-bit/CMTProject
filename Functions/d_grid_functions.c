fb7258139c99db98cda9401c638b6a22c102b470
LCroft22 <lafkieffer@gmail.com>
2024-12-18 23:49:50 +0100

    Update d_grid_functions.c

--- Functions/d_grid_functions.c
+++ Functions/d_grid_functions.c
@@ -67,44 +67,54 @@ void coordinates_adaption(struct Tree *trees, double *x, double *y, int size_tre
         trees[i].position_x_grid = trees_x_converted[i];
         trees[i].position_y_grid = trees_y_converted[i];
     }
+
+    // Free the allocated memory
     free(trees_x_converted);
     free(trees_y_converted);
 }
-//-------------calculate length array--------------
-//Calculates the distance between the min and max of a list
-// divided by our gridsize. We use gridsize = 100, to normalize our trees
-// over fields of squares of 100x100m
-int distance(double* points, int size_trees_array, int gridsize){
+
+// ---------------------------------------------------------------------------------------------------------------------------------
+// distance: This function finds the point with the minimum x and y coordinates and the point with the maximum x and y coordinates.
+//           It then calculates the distance between these two points and divides it by the size of the grid. We use gridsize = 100
+//           to distribute the trees over square fields of 100 x 100 m. The distance is given in meters since we are using the LV95 
+//           coordinate system. 
+// ---------------------------------------------------------------------------------------------------------------------------------
+
+int distance(double *points, int size_trees_array, int gridsize) {
+    // Find the point with the minimum coordinates and the point with the maximum coordinates
     double min_p = min(points, size_trees_array);
     double max_p = max(points, size_trees_array);
+    
+    // Calculate the distance between these points and divide it by gridsize
     return (int)ceil((max_p - min_p)/gridsize);
 }
-// This distance is given in meters, since we're using the LV95 coordinate system
 
-//-------------calculations-----------------------------
-// This function calculates the necessary properties within 
-// a 100x100 square and writes that value to the corresponding 
-// element of our grid
+// ------------------------------------------------------------------------------------------------------------------------------
+// calculations: This function calculates all the properties necessary to find the PM10 deposition, the OFP and the O3 removal. 
+//               It applies the formulas of the model to all the trees present in a 100 x 100 m square field and adds the sum of 
+//               the results to the corresponding grid cell.
+// -------------------------------------------------------------------------------------------------------------------------------
 
-
-
-void calculations(int size_trees_array_filtered_trees, int length_y,int length_x, double** grid_OFP, double** grid_PM10, double** grid_O3, double** grid_O3_net_uptake, struct Tree *trees, double Ci_City, double conc_O3_city, int gridsize){
-    for(int i = 0; i < length_y; i++){
-        for(int j = 0; j<length_x; j++){
+void calculations(int size_trees_array_filtered_trees, int length_y, int length_x, double **grid_OFP, double **grid_PM10, double **grid_O3, double **grid_O3_net_uptake, struct Tree *trees, double conc_PM10_city, double conc_O3_city, int gridsize) {
+    for(int i = 0; i < length_y; i++) {
+        for(int j = 0; j < length_x; j++) {
+            // Initialize the OFP, PM10 and O3 sums for a grid cell to 0
             double OFP_tot = 0.0;
-            double PM_tot = 0.0;
+            double PM10_tot = 0.0;
             double O3_uptake_tot = 0.0;
             double O3_net_uptake_tot = 0.0;
-            for(int k = 0; k<size_trees_array_filtered_trees; k++){
-                if((trees[k].position_x_grid <=j*gridsize+gridsize && trees[k].position_x_grid >=j*gridsize) &&(trees[k].position_y_grid >=i*gridsize && trees[k].position_y_grid <= i*gridsize+ gridsize)){
-                    // functions are defined in a_model_functions.c
+            
+            for(int k = 0; k < size_trees_array_filtered_trees; k++) {
+                if((trees[k].position_x_grid <= j * gridsize + gridsize && trees[k].position_x_grid >= j * gridsize) && (trees[k].position_y_grid >= i * gridsize && trees[k].position_y_grid <= i * gridsize + gridsize)) {
+                    
+                    // Apply the functions defined in a_model_functions.c to each tree present in a square field 
                     leaf_area_func(&trees[k]);
                     leaf_dry_weight_func(&trees[k]);
                     OFP_hourly_func(&trees[k]);
                     OFP_yearly_func(&trees[k]);
                     OFP_tot += trees[k].OFP_yearly;
-                    PM10_yearly_func(&trees[k], Ci_City);
-                    PM_tot += trees[k].PM10_yearly;
+                    PM10_yearly_func(&trees[k], conc_PM10_city);
+                    PM10_tot += trees[k].PM10_yearly;
                     O3_instantaneous_func(&trees[k], conc_O3_city);
                     O3_yearly_func(&trees[k]);
                     O3_removal_yearly_func(&trees[k]);
@@ -114,12 +124,13 @@ void calculations(int size_trees_array_filtered_trees, int length_y,int length_x
                     O3_net_uptake_tot += trees[k].O3_net_uptake_yearly;
                 }
             }
-            // add values to field in grid and conversion to kg/y
-            grid_OFP[i][j] = OFP_tot*pow(10, -9);
-            grid_PM10[i][j] = PM_tot;
-            grid_O3[i][j] = O3_uptake_tot *pow(10,-3);
-            grid_O3_net_uptake[i][j] = O3_net_uptake_tot*pow(10,-3);
-    }
+            
+            // Add the values to the fields in the grid and convert them to kg/y
+            grid_OFP[i][j] = OFP_tot * pow(10, -9);
+            grid_PM10[i][j] = PM10_tot;
+            grid_O3[i][j] = O3_uptake_tot * pow(10,-3);
+            grid_O3_net_uptake[i][j] = O3_net_uptake_tot * pow(10,-3);
+        }
     }
 }
 
